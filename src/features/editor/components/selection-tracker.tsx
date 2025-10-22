0fc50af4a024ee4e48c70079822e3096ce7715fa
mokhtarHamdoune <mokhtardark@gmail.com>
2025-10-22 23:21:55 +0100

    fix(editor): Remove unnecessary rendering logic when selection is cleared
    - This fix the error of ref null when the component is up for the firs time so the toolbare is not above the text untill the second select or move the cursor to the side
    - Added some animation to the toolbar

--- src/features/editor/components/selection-tracker.tsx
+++ src/features/editor/components/selection-tracker.tsx
@@ -20,21 +20,19 @@ export const SelectionTracker = ({
     }
   }, [isTrackingAllowd, disableTracking, allowTracking]);
 
-  // When selection is cleared, render nothing so children will not be shown
-  if (!coordinates) {
-    return null;
-  }
-
   return (
     <div
       ref={ref}
       style={{
         top: (coordinates?.top || 0) - (ref.current?.offsetHeight || 0) - 10,
         left: coordinates?.left || 0,
+        opacity: 1,
       }}
-      className="absolute"
+      className={`absolute z-10 transition duration-100 ease-in ${
+        coordinates ? "translate-y-1" : "-translate-y-1"
+      }`}
     >
-      <div className="relative z-10">{children}</div>
+      <div className="relative">{children}</div>
     </div>
   );
 };

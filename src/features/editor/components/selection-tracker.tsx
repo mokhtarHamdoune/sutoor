b9f3ac1b663387eac3db65e2931cc93bb0b6c6cf
mokhtarHamdoune <mokhtardark@gmail.com>
2025-10-25 23:06:47 +0100

    fix(editor): update visibility of the floating toolabre by making it false when the page render for the first time
    - Hide the toolbare at first render because reference is null which means toolbare shows up in the top left
    
    This is fix the issue of the toolbare shows up top-left when the page render for the first time

--- src/features/editor/components/selection-tracker.tsx
+++ src/features/editor/components/selection-tracker.tsx
@@ -16,9 +16,9 @@ export const SelectionTracker = ({
       style={{
         top: (coordinates?.top || 0) - (ref.current?.offsetHeight || 0) - 10,
         left: coordinates?.left || 0,
-        opacity: 1,
+        visibility: coordinates ? "visible" : "hidden",
       }}
-      className={`absolute z-10 transition duration-100 ease-in ${
+      className={`absolute z-10 transition duration-150 ease-in ${
         coordinates ? "translate-y-1" : "-translate-y-1"
       }`}
     >
